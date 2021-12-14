cebc3e908d76964a20e44013b5482df38601ff2c
muradek <muradek@campus.technion.ac.il>
2021-12-14 13:35:06 +0000

    debugged handlers

--- signals.cpp
+++ signals.cpp
@@ -9,13 +9,14 @@ using namespace std;
 void ctrlZHandler(int sig_num) {
     cout << "smash: got ctrl-Z" << endl;
     if(SmallShell::getInstance().p_running){ // check if a process is running, if so
+        // cout << "were here" << endl;
         pid_t pid_to_kill = SmallShell::getInstance().cur_cmd->p_id; //get the pid to stop and add to jobs list maybe check this? not nullptr?
         SmallShell::getInstance().jobslist.addJob(SmallShell::getInstance().cur_cmd, true);// add it to the jobs list
         int return_val =  kill(pid_to_kill, 19);
         if (return_val){
             perror("smash error: kill failed");
         }
-        cout << "smash: process " << pid_to_kill <<  " was stopped";
+        cout << "smash: process " << pid_to_kill <<  " was stopped" << endl;
         SmallShell::getInstance().p_running = false;
         SmallShell::getInstance().cur_cmd = nullptr;
     }
@@ -29,8 +30,7 @@ void ctrlCHandler(int sig_num) {
         if (return_val){
             perror("smash error: kill failed");
         }
-        cout << "smash: process " << pid_to_kill <<  " was killed";
-        cout << " ctrlC debug: in process: " << getpid() << endl;
+        cout << "smash: process " << pid_to_kill <<  " was killed" << endl;
         SmallShell::getInstance().p_running = false;
         SmallShell::getInstance().cur_cmd = nullptr;
     }
